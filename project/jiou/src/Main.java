cb0abd785d37e134fdaabedd9f33ed86d5f4715b
beijie-z <130464568+beijie-z@users.noreply.github.com>
2023-04-12 23:14:50 +0800

    Add files via upload

---
+++ project/jiou/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int number= input.nextInt();
+       if(number%2==0){
+           System.out.println("偶数");
+       }
+       else {
+           System.out.println("奇数");
+       }
+      if (number % 2 == 0) {
+          System.out.println("函数");
+      }
+    }
+}
